2af9bea4867488718ea8a2913c8b4bc37ed7a62b
bendre90 <benni@bendre.org>
2025-07-08 21:19:23 +0200

    Add AuthOnly middleware tests

--- tests/handlers/auth_test.go
+++ tests/handlers/auth_test.go
@@ -1,1 +1,36 @@
 package handlers
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/daystram/go-gin-gorm-boilerplate/constants"
+	"github.com/daystram/go-gin-gorm-boilerplate/utils"
+)
+
+func TestAuthOnly_Unauthenticated(t *testing.T) {
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+	c.Set(constants.IsAuthenticatedKey, false)
+
+	utils.AuthOnly(c)
+
+	if w.Code != http.StatusUnauthorized {
+		t.Errorf("expected status %d, got %d", http.StatusUnauthorized, w.Code)
+	}
+}
+
+func TestAuthOnly_Authenticated(t *testing.T) {
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+	c.Set(constants.IsAuthenticatedKey, true)
+
+	utils.AuthOnly(c)
+
+	if c.IsAborted() {
+		t.Errorf("middleware aborted despite authenticated context")
+	}
+}
